f50f5494612e15cfeecda4ea8e70b5a62ec8de6a
Subash Bhujel <subashbhujel@live.com>
2014-09-03 22:00:25 -0700

    Adding trick Questions

---
+++ TrickQuestions/TrickQuestions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparation.TrickQuestions
+{
+    class TrickQuestions
+    {
+        /// <summary>
+        /// We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). 
+        /// Return true if it is possible to make the goal by choosing from the given bricks
+        ///     Examples:
+        ///         makeBricks(3, 1, 8) → true, 5+3=8
+        ///         makeBricks(3, 1, 9) → false, 5+3!=9
+        ///         makeBricks(3, 2, 10) → true, 5+5=9
+        ///         makeBricks(2, 4, 8) → false, 5+2!=8
+        /// </summary>
+        /// <param name="small">Small Brick</param>
+        /// <param name="big">Big Brick</param>
+        /// <param name="goal">Goal brick</param>
+        /// <returns></returns>
+        public bool makeBricks(int small, int big, int goal)
+        {
+        }
+    }
+}
