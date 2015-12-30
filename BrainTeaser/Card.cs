4006f50c36833fd3dffff0d6017ce7f9ebdb6a22
Subash Bhujel <subasb@microsoft.com>
2015-12-30 14:35:21 -0800

    Teaser: Find a set of 3 cards with same of different properties. PowerBi team Question @ Microsoft.

---
+++ BrainTeaser/Card.cs
@@ -0,0 +1,56 @@
+﻿namespace InterviewPreparation
+{
+    /// <summary>
+    /// Stores the color of an object in the card. 
+    /// </summary>
+    public enum Colors
+    {
+        Purple,
+        Red,
+        Green
+    }
+
+    /// <summary>
+    /// Shape of the object in the card
+    /// </summary>
+    public enum Shapes
+    {
+        Oval,
+        Round,
+        Diamond
+    }
+
+    /// <summary>
+    /// Stores the shade of the object in the card
+    /// </summary>
+    public enum Shades
+    {
+        None,
+        Solid,
+        Hatch
+    }
+
+    /// <summary>
+    /// Stores the number of objects in the card.
+    /// </summary>
+    public enum ObjCounts
+    {
+        One,
+        Two,
+        Three
+    }
+
+    /// <summary>
+    /// Card object
+    /// </summary>
+    public class Card
+    {
+        public Colors Color { get; set; }
+
+        public Shapes Shape { get; set; }
+
+        public Shades Shade { get; set; }
+
+        public ObjCounts Count { get; set; }
+    }
+}
--- BrainTeaser/Teaser.cs
+++ BrainTeaser/Teaser.cs
@@ -10,6 +10,157 @@ namespace InterviewPreparation
 {
     public class Teaser
     {
+        /// <summary>
+        /// Team: PowerBI Microsoft 12/30/2015
+        /// 
+        /// Problem:
+        ///     Given a number of cards, find a set of 3 card with the following properties:
+        ///      - The property of each card can be all same or all different. For Eg:
+        ///                     Number Of Obj       Color       Shape       Shade
+        ///                     -------------------------------------------------------
+        ///         - Card 1:       1               Purple      Oval        Solid
+        ///         - Card 2:       1               Red         Oval        Hatch
+        ///         - Card 3:       1               Green       Oval        None
+        ///         Note: Correct Set. Same number, Shape and all different color, shade
+        ///         
+        ///         - Card 1:       1               Purple      Oval        Solid
+        ///         - Card 2:       2               Red         Diamond     Solid
+        ///         - Card 3:       3               Green       Round       Solid
+        ///         Note: Correct Set. Same Shade and all different num, color, shape
+        ///         
+        ///         - Card 1:       1               Purple      Oval        Solid
+        ///         - Card 2:       1               Red         Diamond     Solid
+        ///         - Card 3:       3               Green       Round       Solid
+        ///         Note: Incorrect Set. Two same number of obj and one diff.
+        ///                 Has to be all same or all different.
+        ///         
+        ///         - Card 1:       1               Purple      Oval        Solid
+        ///         - Card 2:       1               Red         Diamond     Solid
+        ///         - Card 3:       1               Purple      Round       Solid
+        ///         Note: Incorrect Set. Reason - color - two same and one different. 
+        ///                 Has to be all same or all different.
+        ///                 
+        /// Note: 
+        ///     Each property can have only 3 values. Look into enums under Card.cs class
+        ///     
+        /// 
+        /// APPROACH: 
+        ///     - Pick a first element
+        ///     - loop through the rest to pick its pair one by one
+        ///     - grab all of the properties from step 1 and 2. Now you know exactly what element to look for
+        ///             With what exact properties.
+        ///     - loop through the array again, to pick it's thrid set that matches the properties above.
+        ///     - If found, there's your first set.
+        ///     - Else, continue the loop.
+        ///     - If no set can be formed at the end of first loop, return null.
+        /// </summary>
+        /// <param name="cards"></param>
+        public void FindASetOf3Cards(Card[] cards)
+        {
+            // Check for null and less number of cards
+            if (cards.Length <= 2 || cards == null)
+            { return; }
+
+            List<Card> set = new List<Card>();
+
+            Card a = new Card();
+            Card b = new Card();
+            Card c = new Card();
+
+            // Loop through each items. First item.
+            for (int i = 0; i < cards.Length; i++)
+            {
+                // Your first item
+                a = cards[i];
+
+                // Loop through each items. Second item.
+                for (int j = 0; j < cards.Length; j++)
+                {
+                    // Ran into same item. Skip.
+                    if (j == i) continue;
+
+                    // your seond item for the set
+                    b = cards[j];
+
+                    // Evaluate properties for first 2 items.
+                    Colors color;
+                    if (a.Color == b.Color) color = a.Color;
+                    else
+                    {
+                        if (a.Color == Colors.Green && b.Color == Colors.Purple)
+                            color = Colors.Red;
+                        else if (a.Color == Colors.Green && b.Color == Colors.Red)
+                            color = Colors.Purple;
+                        else if (a.Color == Colors.Purple && b.Color == Colors.Green)
+                            color = Colors.Red;
+                        else if (a.Color == Colors.Purple && b.Color == Colors.Red)
+                            color = Colors.Green;
+                        else if (a.Color == Colors.Green && b.Color == Colors.Purple)
+                            color = Colors.Red;
+                        else
+                            color = Colors.Purple;
+                    }
+
+                    // Similarly find the shade, shape, numberofObjs peoperty.
+                    // These are the property you will be looking for in the 3rd and last element to complete the set.
+
+                    // NOTE: these are wrong assumption. Just initializing.
+                    //  Read 2 lines above what you need to do. I'm being lazy here. :)
+                    Shades shade = Shades.Hatch;
+                    Shapes shape = Shapes.Diamond;
+                    ObjCounts number = ObjCounts.One;
+
+                    // Loop through each items. Third item.
+                    for (int k = 0; k < cards.Length; k++)
+                    {
+                        // Ran into either first or second item. Skip.
+                        if (k == i || k == j) continue;
+
+                        // Your third item for the set.
+                        c = cards[k];
+
+                        // Check if this is the RIGHT 3rd item.
+                        if (c.Color == color && c.Shade == shade && c.Shape == shape && c.Count == number)
+                        {
+                            // This is your first set of card. You can break or return here if you want just the
+                            // First set of 3 cards.
+                            // If you want all permutations of cards, keep going.
+
+                            set.Add(a);
+                            set.Add(b);
+                            set.Add(c);
+                        } // If ends
+                    }// For ends
+                }// For ends
+            }// For ends
+
+            // No set found. Return.
+            if (set.Count == 0) return;
+
+            int count = 0;
+
+            // Print the sets.
+            foreach (var item in set)
+            {
+                if (count == 0)
+                {
+                    Console.WriteLine("First set:");
+                }
+
+                // Print 3 items in batch.
+                if (count < 3)
+                {
+                    Console.Write("Color: {0} | Shape: {1} | Shade: {2} | Number: {3}", item.Color, item.Shape, item.Shade, item.Count);
+                    count++;
+
+                    // Reset
+                    if (count == 3) count = 0;
+                }
+
+            }
+
+
+        }
 
         /// <summary>
         /// Given a sorted array and a number x, find the pair in array whose sum is closest to x
